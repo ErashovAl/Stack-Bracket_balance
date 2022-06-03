d9c33ba48402c4036d9173483593eb74a06e2222
ErashovAl <erashov.al@mail.ru>
2022-06-03 20:46:02 +0300

    Start commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from stack import Stack
+
+st = Stack()
+
+def brackets(text, pattern = '(){}[]'):
+ 
+    opened, closed = pattern[::2], pattern[1::2]
+    if text == '':
+        return False
+
+    if text[0] in closed:# or len(text) %2
+        return False
+
+    for el in text:
+        if el in opened:
+            st.push(opened.index(el))
+        elif el in closed:
+            if st.stack[-1] == closed.index(el):
+                st.pop()
+            else: return False
+
+    return True
+
+
+if __name__ == '__main__':
+    
+    sequence = input('Введите последовательность: ')
+
+    if brackets(sequence):
+        print('"Сбалансированно"')
+    else:
+        print('"Несбалансированно"')
---
+++ stack.py
@@ -0,0 +1,27 @@
+
+class Stack():
+    def __init__(self) -> None:
+        self.stack = []
+
+    def is_empty(self):
+        if len(self.stack) == 0:
+            return True
+        else: return False
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        if len(self.stack) == 0:
+            return None
+        removed = self.stack.pop()
+        return removed
+
+    def peek(self):
+        if len(self.stack) == 0:
+            return None
+        return self.stack[-1]
+
+    def size(self):
+        size = len(self.stack)
+        return size
